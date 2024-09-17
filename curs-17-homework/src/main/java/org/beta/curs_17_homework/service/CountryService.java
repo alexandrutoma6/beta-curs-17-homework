49243e3a6587cf9543badd9df373d8a65ad67169
Toma Alexandru <alextoma73.at@gmail.com>
2024-09-17 07:55:00 +0300

    Return Optional instead of throwing error

--- curs-17-homework/src/main/java/org/beta/curs_17_homework/service/CountryService.java
+++ curs-17-homework/src/main/java/org/beta/curs_17_homework/service/CountryService.java
@@ -4,6 +4,7 @@ import org.beta.curs_17_homework.model.Country;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -24,21 +25,19 @@ public class CountryService {
                 .toList();
     }
 
-    public String getCountryCapital(String countryName) {
+    public Optional<String> getCountryCapital(String countryName) {
         return countries.stream()
-                .filter(country -> country.name().equals(countryName))
+                .filter(country -> country.name().equalsIgnoreCase(countryName))
                 .findFirst()
-                .map(Country::capital)
-                .orElseThrow(() -> new IllegalArgumentException("Country not found: " + countryName));
+                .map(Country::capital);
     }
 
 
-    public long getCountryPopulation(String countryName) {
+    public Optional<Long> getCountryPopulation(String countryName) {
         return countries.stream()
                 .filter(country -> country.name().equals(countryName))
                 .findFirst()
-                .map(Country::population)
-                .orElseThrow(() -> new IllegalArgumentException("Country not found: " + countryName));
+                .map(Country::population);
     }
 
     public List<Country> getCountryFromContinent(String continent) {
@@ -47,12 +46,11 @@ public class CountryService {
                 .toList();
     }
 
-    public List<String> getCountryNeighbours(String countryName) {
+    public Optional<List<String>> getCountryNeighbours(String countryName) {
         return countries.stream()
                 .filter(country -> country.name().equals(countryName))
                 .findFirst()
-                .map(Country::neighbours)
-                .orElseThrow(() -> new IllegalArgumentException("Country not found: " + countryName));
+                .map(Country::neighbours);
     }
 
     public List<Country> getCountriesWithPopulationFromContinentLargerThan(String continent, long population) {
@@ -64,8 +62,11 @@ public class CountryService {
 
     public List<Country> getCountriesWithNeighbours(String neighbour, String notNeighbour) {
         return countries.stream()
-                .filter(country -> country.neighbours().contains(neighbour) && !(country.neighbours().contains(notNeighbour)))
+                .filter(country -> country.neighbours().contains(neighbour))
+                .filter(country -> !country.name().equals(notNeighbour))
+                .filter(country -> !country.neighbours().contains(notNeighbour))
                 .toList();
     }
+
 
 }
